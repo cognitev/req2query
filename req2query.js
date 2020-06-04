7b7a5a18d80fbaa2de28c74bc03b151f4d86b992
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2020-06-04 16:34:25 +0200

    req2query

---
+++ req2query.js
@@ -0,0 +1,94 @@
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
+const generator = function(modelAttr) {
+  return (req, res, next) => {
+    const extractedAtrr = extractAttr(req, modelAttr);
+    req.sequelizeQuery = matchAttr(extractedAtrr);
+
+    next();
+  };
+};
+
+const extractAttr = function(req, modelAttr) {
+  const extractedAttr = {};
+
+  const { query } = req;
+
+  for (let propertyName in query) {
+    let [key, op] = propertyName.split('__');
+
+    const supportedOPerations = ['gte', 'gt', 'lte', 'lt'].includes(op);
+
+    if (!supportedOPerations && typeof op !== 'undefined') continue;
+
+    if (modelAttr[key]) {
+      const model = modelAttr[key]['model'];
+
+      if (!extractedAttr[model]) extractedAttr[model] = {};
+
+      if (op) {
+        const extract = { value: query[propertyName], op };
+
+        if (extractedAttr[model][key]) {
+          extractedAttr[model][key].push(extract);
+        } else {
+          extractedAttr[model][key] = [extract];
+        }
+      } else {
+        if (modelAttr[key]['type'] === 'Boolean') {
+          extractedAttr[model][key] = [JSON.parse(query[key])];
+        } else {
+          extractedAttr[model][key] = (Array.isArray(query[key])) ? query[key] : [query[key]];
+        }
+      }
+    }
+  }
+  return extractedAttr;
+};
+
+const matchAttr = function(extractedAttr) {
+  const matchedObj = {};
+
+  for (let modelName in extractedAttr) {
+    const model = extractedAttr[modelName];
+
+    let where = {};
+    for (let propertyName in model) {
+      if (typeof model[propertyName][0] !== 'object') {
+        where[propertyName] = model[propertyName];
+      } else {
+        if (model[propertyName].length === 1){
+          where[propertyName] = getOPerator(model[propertyName][0]);
+        } else {
+          where[propertyName] = getAndOperator(model[propertyName]);
+        }
+      }
+
+      matchedObj[modelName] = { where };
+    }
+  }
+  return matchedObj;
+};
+
+function getOPerator(property) {
+  return {
+    [Op[property.op]]: property.value,
+  };
+}
+
+function getAndOperator(properties) {
+  const condidtions = [];
+
+  properties.forEach(property => {
+    condidtions.push(getOPerator(property));
+  });
+
+  return { [Op.and]: condidtions };
+}
+
+module.exports = {
+  generator,
+  extractAttr,
+  matchAttr,
+};
---
+++ test_req2query.js
@@ -0,0 +1,377 @@
+const assert = require("assert");
+const extractAttr = require("./req2query").extractAttr;
+const modelAttr = {
+  started_at: {
+    type: "Date",
+    model: "campaign",
+  },
+  amount: {
+    type: "Number",
+    model: "package",
+  },
+  is_deleted: {
+    type: "Boolean",
+    model: "campaign",
+  },
+  state: {
+    type: "String",
+    model: "package",
+  },
+  created_at: {
+    type: "Date",
+    model: "campaign",
+  },
+  balance: {
+    type: "Number",
+    model: "package",
+  },
+  is_ga_integrated: {
+    type: "Boolean",
+    model: "campaign",
+  },
+};
+
+let currentTest;
+try {
+  /* TESTCASE */
+  currentTest = "base case no-filteration";
+  console.log(currentTest);
+
+  let extractedAttr = extractAttr({ query: {} }, modelAttr);
+  assert.deepEqual(extractedAttr, {});
+  
+  /* TESTCASE */
+  currentTest = "1. one query parameter";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr({ query: { state: "RUNNING" } }, modelAttr);
+  assert.deepEqual(extractedAttr, { package: { state: ["RUNNING"] } });
+  
+  /* TESTCASE */
+  currentTest = "2. non existing attr";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    { query: { state: "RUNNING", size: 10 } },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, { package: { state: ["RUNNING"] } });
+  
+  /* TESTCASE */
+  currentTest = "3. multi value attr";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    { query: { state: ["RUNNING", "PENDING", "PENDING_PAID"] } },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: { state: ["RUNNING", "PENDING", "PENDING_PAID"] },
+  });
+
+  
+  /* TESTCASE */
+  currentTest = "4. add another query parameter";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    {
+      query: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: "10",
+      },
+    },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: ["10"],
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "5. add another query parameters from another model";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    {
+      query: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: "10",
+        started_at: "2020-01-01",
+        is_deleted: true,
+      },
+    },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: ["10"],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "6. add amount gte operator";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    {
+      query: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount__gte: "10",
+        started_at: "2020-01-01",
+        is_deleted: true,
+      },
+    },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: [{ value: 10, op: "gte" }],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "7. add amount gte,lte operator";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    {
+      query: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount__gte: "10",
+        amount__lte: "20",
+        started_at: "2020-01-01",
+        is_deleted: true,
+      },
+    },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: [
+        { value: 10, op: "gte" },
+        { value: 20, op: "lte" },
+      ],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "8. invalid test operator";
+  console.log(currentTest);
+
+  extractedAttr = extractAttr(
+    {
+      query: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount__gta: "10",
+        amount__lte: "20",
+        started_at: "2020-01-01",
+        is_deleted: true,
+      },
+    },
+    modelAttr
+  );
+  assert.deepEqual(extractedAttr, {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: [{ value: 20, op: "lte" }],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  });
+
+  const matchedObject = require("./req2query").matchAttr;
+  const sequelize = require("sequelize");
+  const Op = sequelize.Op;
+  
+  /* TESTCASE */
+  currentTest = "MATCHEDOBJECT: base case empty query";
+  console.log(currentTest);
+
+  extractedAttr = {};
+  let matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {});
+  
+  /* TESTCASE */
+  currentTest = "1: one query single parameter";
+  console.log(currentTest);
+
+  extractedAttr = { package: { state: ["RUNNING"] } };
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING"],
+      },
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "2: one query array parameter";
+  console.log(currentTest);
+
+  extractedAttr = {
+    package: { state: ["RUNNING", "PENDING", "PENDING_PAID"] },
+  };
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      },
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "3: two query parameters";
+  console.log(currentTest);
+
+  extractedAttr = {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: ["10"],
+    },
+  };
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: ["10"],
+      },
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "4: add query parameters from another model";
+  console.log(currentTest);
+
+  extractedAttr = {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: ["10"],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  };
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: ["10"],
+      },
+    },
+    campaign: {
+      where: {
+        started_at: ["2020-01-01"],
+        is_deleted: [true],
+      },
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "5. add amount gte operator";
+  console.log(currentTest);
+
+  extractedAttr = {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: [{ value: 10, op: "gte" }],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  };
+
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: {
+          [Op.gte]: 10,
+        },
+      },
+    },
+    campaign: {
+      where: {
+        started_at: ["2020-01-01"],
+        is_deleted: [true],
+      },
+    },
+  });
+  
+  /* TESTCASE */
+  currentTest = "6. add amount gte, lte operator";
+  console.log(currentTest);
+
+  extractedAttr = {
+    package: {
+      state: ["RUNNING", "PENDING", "PENDING_PAID"],
+      amount: [
+        { value: 10, op: "gte" },
+        { value: 20, op: "lte" },
+      ],
+    },
+    campaign: {
+      started_at: ["2020-01-01"],
+      is_deleted: [true],
+    },
+  };
+
+  matchedObj = matchedObject(extractedAttr);
+  assert.deepEqual(matchedObj, {
+    package: {
+      where: {
+        state: ["RUNNING", "PENDING", "PENDING_PAID"],
+        amount: {
+          [Op.or]: {
+            [Op.gte]: 10,
+            [Op.lte]: 20,
+          },
+        },
+      },
+    },
+    campaign: {
+      where: {
+        started_at: ["2020-01-01"],
+        is_deleted: [true],
+      },
+    },
+  });
+
+  
+  /* TESTCASE */
+  currentTest = "7. return generator function";
+  console.log(currentTest);
+
+  const generatorFn = require("./req2query").generator();
+
+  assert.equal(typeof generatorFn, "function");
+
+} catch (ex) {
+  console.log(ex);
+}
